f88b03514020ccfeb7c5503df958a6e20b7e2062
m2rdikas <vellak456@hotmail.com>
2013-02-22 11:33:47 +0200

    smth

---
+++ lambiprojekt123/src/ut/veeb/lambiprojekt123/Lambiprojekt123Servlet.java
@@ -0,0 +1,13 @@
+package ut.veeb.lambiprojekt123;
+
+import java.io.IOException;
+import javax.servlet.http.*;
+
+@SuppressWarnings("serial")
+public class Lambiprojekt123Servlet extends HttpServlet {
+	public void doGet(HttpServletRequest req, HttpServletResponse resp)
+			throws IOException {
+		resp.setContentType("text/plain");
+		resp.getWriter().println("Hello, world");
+	}
+}
