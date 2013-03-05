2eb9a15c94106863d8536f38fd336d63d91a0c09
m2rdikas <vellak456@hotmail.com>
2013-03-05 11:45:56 +0200

    Lihtne valideerimine

--- lambiprojekt123/war/js/scripts.js
+++ lambiprojekt123/war/js/scripts.js
@@ -11,3 +11,23 @@ function setDisplayNone(){
 	document.getElementById('realTime').style.display="none";
 	document.getElementById('statistics').style.display="none";
 }
+function validateNotEmpty(elemId){
+	var elem = document.getElementById(elemId);
+	if(document.getElementById(elemId).value == ""){
+		document.getElementById(elemId).className="error";
+		document.getElementById(elemId + "Label").className="error";
+		document.getElementById(elemId + "Error").className="error";
+		document.getElementById(elemId + "Error").innerHTML = "Väli on kohustuslik";
+		return false;
+	} else {
+		document.getElementById(elemId).className="";
+		document.getElementById(elemId + "Label").className="";
+		document.getElementById(elemId + "Error").innerHTML="";
+		return true;
+	}
+}
+function setCandidate(){
+	validateNotEmpty('firstName');
+	validateNotEmpty('lastName');
+	validateNotEmpty('party');
+}
