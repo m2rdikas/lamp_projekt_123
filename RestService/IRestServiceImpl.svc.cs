13f662665f47582c5545a4b6d94c80dbed18a98a
allarvallaots <valat100@gmail.com>
2013-03-20 15:41:53 +0200

    REST päringute test

---
+++ RestService/IRestServiceImpl.svc.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+namespace rest
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the class name "RestServiceImpl" in code, svc and config file together.
+    public class RestServiceImpl : IRestServiceImpl
+    {
+        #region
+        public string JSONData(string id)
+        {
+            return "Teie soovitud andmed" + id;
+        }
+
+        #endregion
+    }
+}
